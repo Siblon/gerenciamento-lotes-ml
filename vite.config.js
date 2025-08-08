5dc9ca8571f33cf914e081d2ce89e542c1e325f5
Siblon <mtf.milton.tavares@gmail.com>
2025-08-08 17:31:24 -0300

    chore: ignorar node_modules e preparar push

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  root: './public',
   server: {
-    port: 3000
+    port: 5173,
+    strictPort: true,
+    proxy: {
+      '/api': 'http://localhost:3000'
+    }
   }
 })
