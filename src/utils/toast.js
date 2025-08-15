dd02879ecac53f19d7e7e1f70dac34a842266b24
Siblon <mtf.milton.tavares@gmail.com>
2025-08-15 11:13:44 -0300

    feat: update scanner to use zxing package

---
+++ src/utils/toast.js
@@ -0,0 +1,14 @@
+export function toast(msg, type = 'info') {
+  const el = document.createElement('div');
+  el.className = `toast ${type}`;
+  el.textContent = msg;
+  document.body.appendChild(el);
+  setTimeout(() => el.remove(), 2200);
+}
+
+toast.info = (msg) => toast(msg, 'info');
+toast.warn = (msg) => toast(msg, 'warn');
+toast.error = (msg) => toast(msg, 'error');
+toast.success = (msg) => toast(msg, 'success');
+
+export default toast;
